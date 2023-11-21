4a4e7e3a64d4b61672b39d2eb94e32e3ca060bb1
Fonta97 <fontana.alessandro97@gmail.com>
2023-11-21 16:34:00 +0100

    javascript BuzzFizz fatto (incredulo!!!)

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
-for (var i = 1; i <= 62; i++) {
-    if ((i % 3)) === 0{
-        output += "Fizz";
+let output = 0
+
+for (var i = 1; i <= 100; i++) {
+    if ((i % 15) === 0) {
+        output = "BuzzFizz";
+        console.log(output);
     }
-    if ((i % 5)) === 0{
-        output === "Buzz"
+
+    if ((i % 3) === 0) {
+        output = "Fizz";
+        console.log(output);
     }
+    if ((i % 5) === 0) {
+        output === "Buzz";
+        console.log(output);
+    }
+    output = i;
+    console.log(output);
+
 }
