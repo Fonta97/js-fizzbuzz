f00cd0fbd6f580af8316061faf383a1cbd067dd9
Fonta97 <fontana.alessandro97@gmail.com>
2023-11-21 17:16:33 +0100

    errore con append

--- js/script.js
+++ js/script.js
@@ -1,19 +1,32 @@
-let output = 0
+let output = 0;
+
+let div = document.querySelector("box");
 
 for (var i = 1; i <= 100; i++) {
+
+    let square = document.createElement("div");
+    square.classList.add("quadrato");
+    div.append(square);
+
     if ((i % 15) === 0) {
+
         output = "BuzzFizz";
         console.log(output);
-    }
-    else if ((i % 3) === 0) {
+
+    } else if ((i % 3) === 0) {
+
         output = "Fizz";
         console.log(output);
-    }
-    else if ((i % 5) === 0) {
-        output === "Buzz";
+
+    } else if ((i % 5) === 0) {
+
+        output = "Buzz";
         console.log(output);
+
     }
+
     output = i;
     console.log(output);
 
 }
+
