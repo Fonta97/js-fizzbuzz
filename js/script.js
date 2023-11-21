11d7d08535d4aee49c7f5dc7114d0d6ef23f907d
Fonta97 <fontana.alessandro97@gmail.com>
2023-11-21 17:49:44 +0100

    quasi finito, mancano i background di colore diver

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let output = 0;
 
-let div = document.querySelector("box");
+let div = document.querySelector(".box");
 
 for (let i = 1; i <= 100; i++) {
 
@@ -11,22 +11,29 @@ for (let i = 1; i <= 100; i++) {
     if ((i % 15) === 0) {
 
         output = "BuzzFizz";
+        square.append(output);
         console.log(output);
 
     } else if ((i % 3) === 0) {
 
         output = "Fizz";
+        square.append(output);
         console.log(output);
 
     } else if ((i % 5) === 0) {
 
         output = "Buzz";
+        square.append(output);
+        console.log(output);
+
+    } else {
+
+        output = i;
+        square.append(output);
         console.log(output);
 
     }
 
-    output = i;
-    console.log(output);
 
 }
 
